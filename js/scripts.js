af821d5017e4edffe686420ceffac82379b7c172
ShinnyD <shinobuduerloo@gmail.com>
2023-07-31 12:52:33 +0200

    adds buttons with querySelectors and images instead of text git status

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,40 @@
 //create arr with Rock, Paper, Scissors
 let arr = ["Rock", "Paper","Scissors"];
 
-// one round of rock paper scissors
+const buttons = document.querySelectorAll("button");
+
+
 function round() {
-    //getComputerChoice();
+    
     let getComputerChoice = arr[(Math.floor(Math.random() * arr.length))]
-    //set playerSelection
-    let playerSelection= prompt("Please input either Rock, Paper, or Scissors.","")
+  
+    let playerSelection= buttons.className//prompt("Please input either Rock, Paper, or Scissors.","")
     
     console.log("Computer chose " + getComputerChoice);
     console.log("Player chose " + playerSelection);
-//make input case insensitive
-    getComputerChoice = getComputerChoice.toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
 
-    // player win condition
-    if ((playerSelection == "rock" && getComputerChoice == "scissors") || (playerSelection == "paper" && getComputerChoice == "rock") || (playerSelection == "scissors" && getComputerChoice == "paper")){
+    getComputerChoice = getComputerChoice.toLowerCase();
+    //playerSelection = playerSelection.toLowerCase();
+
+    
+    if ((playerSelection == (buttons.className == btnRock) && getComputerChoice == "scissors") || 
+        (playerSelection == (buttons.className == btnPaper) && getComputerChoice == "rock") || 
+        (playerSelection == (buttons.className == btnScissors) && getComputerChoice == "paper")){
         console.log("You win! " + playerSelection + " beats " + getComputerChoice);
         playerScore = playerScore + 1;
         
-    //player lose condition
+    
     } else if ((playerSelection == "scissors" && getComputerChoice == "rock") || (playerSelection == "rock" && getComputerChoice == "paper") || (playerSelection == "paper" && getComputerChoice == "scissors")){
         console.log("You loose! " + getComputerChoice + " beats " + playerSelection);
         computerScore = computerScore + 1;
         
-    //player draw condition
+   
     } else 
     console.log("Its a tie!");
 
 }
 
-//start a round
+
 
 let playerScore = 0;
 let computerScore = 0;
